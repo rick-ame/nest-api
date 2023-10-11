0e5bc989301c6eaf9bd186d549c19348d1e66ee0
Rick <rick.ast@outlook.com>
2023-10-11 16:35:16 +0800

    add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
+import { version } from '../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setVersion(version)
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
