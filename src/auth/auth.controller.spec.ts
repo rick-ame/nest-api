4d45fa27d6c1b75ab1205edc3612043d7bc63cb8
Rick <rick.ast@outlook.com>
2023-11-04 09:14:59 +0800

    add auth module structure

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,27 @@
+import { Test } from '@nestjs/testing'
+
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+
+describe('AuthController', () => {
+  let authController: AuthController
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile()
+
+    authController = app.get(AuthController)
+  })
+
+  describe('auth', () => {
+    it('should user signup', () => {
+      expect(authController.signup()).toEqual('signup')
+    })
+
+    it('should user login', () => {
+      expect(authController.login()).toEqual('login')
+    })
+  })
+})
