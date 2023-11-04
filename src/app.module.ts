87bd022a8462aa31309931f5d514fded42bc06bf
Rick <rick.ast@outlook.com>
2023-11-04 12:15:42 +0800

    add prisma for db (#12)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Controller()
 class AppController {
@@ -14,7 +15,7 @@ class AppController {
 }
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule],
   controllers: [AppController],
 })
 export class AppModule {}
