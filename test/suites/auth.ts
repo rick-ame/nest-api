f186fcf36791cbeb97cf38a242be361e32fdb433
Rick <rick.ast@outlook.com>
2023-11-07 17:54:16 +0800

    run e2e tests in sequential (#14)

---
+++ test/suites/auth.ts
@@ -0,0 +1,31 @@
+import * as request from 'supertest'
+
+import { fixture as f } from '../helper'
+
+export default () =>
+  describe('AuthController (e2e)', () => {
+    const user = {
+      email: 'foo@bar.com',
+      password: '123',
+    }
+
+    it('/signup (POST)', () => {
+      return request(f.app.getHttpServer())
+        .post('/signup')
+        .send(user)
+        .expect(201)
+        .expect(({ body }) => {
+          expect(body.email).toBe(user.email)
+        })
+    })
+
+    it('/login (POST)', () => {
+      return request(f.app.getHttpServer())
+        .post('/login')
+        .send(user)
+        .expect(201)
+        .expect(({ body }) => {
+          expect(body.email).toBe(user.email)
+        })
+    })
+  })
